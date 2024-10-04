1f312a481bf949e8c249fbc09e1ff57274af6bca
bogdanafilimonenko <bfilimonenko@gmail.com>
2024-10-04 23:37:07 +0300

    feature: adaptive, menu, animation

--- js/main.js
+++ js/main.js
@@ -1,25 +1,15 @@
-let burger = document.getElementById("burger");
+const burger = document.getElementById("burger");
 
-let navbarList = document.getElementById("navbar");
-console.log(burger);
-console.log(navbarList);
+const navbarList = document.getElementById("navbarList");
+
 
 
 burger.addEventListener("click", function () {
     burger.classList.toggle('active');
-    navbarList.style.display = "block";
     if (navbarList.style.display === "block") {
         navbarList.style.display = "none"
     } else navbarList.style.display = "block"
 })
 
 
-let burger = document.querySelector("#burger");
-console.log(burger);
-let navbarList = document.querySelector("#navbarList");
-console.log(navbarList);
 
-burger.addEventListener("click", function () {
-    burger.classList.toggle("active");
-    navbarList.classList.toggle("navbar-list--active");
-})
