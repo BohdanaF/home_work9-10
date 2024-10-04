4592ec8b2f8c19acbd8a245253299d4dac2464bf
bogdanafilimonenko <bfilimonenko@gmail.com>
2024-10-05 00:15:20 +0300

    fix: incorrect menu logic on resize

--- js/main.js
+++ js/main.js
@@ -2,14 +2,9 @@ const burger = document.getElementById("burger");
 
 const navbarList = document.getElementById("navbarList");
 
-
-
 burger.addEventListener("click", function () {
     burger.classList.toggle('active');
     if (navbarList.style.display === "block") {
         navbarList.style.display = "none"
     } else navbarList.style.display = "block"
 })
-
-
-
